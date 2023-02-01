64e2ce1395fa45cb263a0b724eba1eaf30b9b0a7
patelheta <patelheta385@gmail.com>
2023-02-01 00:03:57 -0800

    Fix build failed issue

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,19 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Bungee|Source+Sans+Pro:300,400,600"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
-          crossorigin="anonymous"
-        />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
